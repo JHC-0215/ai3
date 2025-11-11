ee464e268b4b65fd3349bafd9c7527ef509dd00e
JHC-0215 <h098s02820@chunchon.gwe.hs.kr>
2025-11-11 14:20:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
        "texts": ["막스가 있다."],
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
     #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
-    # },
+     },
 }
 
 # ======================
